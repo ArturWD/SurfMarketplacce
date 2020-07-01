69e6eb4c720a19361914a32a429247106fe3a0b9
Artur Shestakov <shestakovartur2.0@gmail.com>
2020-07-01 16:58:23 +0500

    user routes

--- surf-marketplace/routes/index.js
+++ surf-marketplace/routes/index.js
@@ -6,4 +6,40 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'SurfShop Home' });
 });
 
+router.get('/register', (req, res, next) => {
+  res.send('register get');
+});
+router.post('/register', (req, res, next) => {
+  res.send('register post');
+});
+router.get('/login', (req, res, next) => {
+  res.send('login get');
+});
+router.post('/login', (req, res, next) => {
+  res.send('login post');
+});
+
+router.get('/profile', (req, res, next) => {
+  res.send('profile get');
+});
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('register post');
+});
+
+router.get('/forgot-password', (req, res, next) => {
+  res.send('profile get');
+});
+
+router.put('/forgot-password', (req, res, next) => {
+  res.send('profile get');
+});
+
+router.get('/reset-password/:token', (req, res, next) => {
+  res.send('profile get');
+});
+
+router.put('/reset-password/:token', (req, res, next) => {
+  res.send('profile get');
+});
+
 module.exports = router;
